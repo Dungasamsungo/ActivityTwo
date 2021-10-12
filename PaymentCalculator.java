db78a0289e983fe8cf265641bd884e9bffcd52f2
sampson <sampsonngobi@gmail.com>
2021-10-11 22:29:45 -0300

    Added a new file "PaymentCalculator" Everything is set but one variable "inpute" can not be resolved.

---
+++ PaymentCalculator.java
@@ -0,0 +1,42 @@
+import javax.swing.JOptionPane;
+
+public class PaymentCalculator{
+    public static void main(String[] args) {
+        
+        /*Write a program to read the value of a product, the system will
+         present 2 options for payment, the value to
+        view with 10% discount, or the value in 3 installments without interest.*/ 
+
+        String productValue;
+        int paymentOption;
+        int input;
+        double discount ;
+        double instantPaymentPrice;
+        double installmentPaymentPrice;
+
+        discount = input* 0.1;
+        instantPaymentPrice = input - discount;
+        installmentPaymentPrice = input / 3;
+        
+        productValue = JOptionPane.showInputDialog(null, "Inpute Product Price");
+         input = Integer.parseInt(productValue);
+
+        Object[] payOptions = {"Instant", "Inatalment Payment"};
+        paymentOption = JOptionPane.showOptionDialog(null,
+         "Choose a payment Option", "Payment Options",
+         JOptionPane.DEFAULT_OPTION,
+         JOptionPane.QUESTION_MESSAGE, null, payOptions, payOptions[1]);
+
+        if (paymentOption==0){
+            JOptionPane.showMessageDialog(null,"Total Payment " + installmentPaymentPrice,
+             "Instant Payment Price", JOptionPane.PLAIN_MESSAGE);
+        }
+        else {
+            JOptionPane.showMessageDialog(null,"Total Payment " + installmentPaymentPrice,
+             "Three Instalment Payment Price", JOptionPane.PLAIN_MESSAGE);  
+        }
+    }
+
+    
+
+}
